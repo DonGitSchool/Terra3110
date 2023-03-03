9a4747ea8d95bdc634ebc3e8e94db35dff26e8e9
DonGitSchool <92173334+DonGitSchool@users.noreply.github.com>
2023-03-02 21:41:21 -0700

    Update Assignment8.tf

--- Assignment8.tf
+++ Assignment8.tf
@@ -118,7 +118,7 @@ resource "aws_instance" "prod" {
   ami           = "ami-0557a15b87f6559cf"
   instance_type = "t2.micro"
   tags = {
-        Name = "dev"
+        Name = "Prod"
   }
   vpc_security_group_ids = [aws_security_group.tf-sg.id]
   associate_public_ip_address = "true"
