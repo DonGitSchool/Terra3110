d94764a34565d4b4970be4774bc3643bf0177531
DonGitSchool <92173334+DonGitSchool@users.noreply.github.com>
2023-03-23 23:37:52 -0600

    Update Assignment8.tf

--- Assignment8.tf
+++ Assignment8.tf
@@ -116,7 +116,7 @@ resource "aws_ebs_volume" "test-vol" {
     }
 }
 resource "aws_volume_attachment" "test-vol" {
-  device_name = "/dev/sdt"
+  device_name = "/dev/sdd"
   volume_id = "${aws_ebs_volume.test-vol.id}"
   instance_id = "${aws_instance.test.id}"
 }
@@ -148,7 +148,7 @@ resource "aws_ebs_volume" "prod-vol" {
     }
 }
 resource "aws_volume_attachment" "prod-vol" {
-  device_name = "/dev/sdp"
+  device_name = "/dev/sdd"
   volume_id = "${aws_ebs_volume.prod-vol.id}"
   instance_id = "${aws_instance.prod.id}"
 }
